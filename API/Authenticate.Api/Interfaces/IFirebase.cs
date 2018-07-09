a0af9d93ac4b712646e28a16e9a437a0bd31d7d6
freedomkhanyile <Freedom.Khanyile1@gmail.com>
2018-07-09 23:24:21 +0200

    Included Firebase interface

---
+++ API/Authenticate.Api/Interfaces/IFirebase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Authenticate.Api.Interfaces
+{
+    public interface IFirebase
+    {
+        List<string> GetUsers();
+    }
+}
