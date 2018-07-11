9d1ff7e11ec44f3ddaf79ff977070916336af5d3
freedomkhanyile <Freedom.Khanyile1@gmail.com>
2018-07-12 00:03:41 +0200

    Login Component

--- WEB/web-app/src/app/account/login/login.component.ts
+++ WEB/web-app/src/app/account/login/login.component.ts
@@ -1,16 +1,52 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
-
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { first } from 'rxjs/operators';
+import { Router, ActivatedRoute } from '@angular/router';
+import { AuthenticationService } from '../../_services';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup
+  loading = false
+  submitted = false
+  returnUrl: string
+  error = ''
 
- 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router,
+    private authenticationService: AuthenticationService
+  ) { }
 
-  ngOnInit() {  }
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required]
+    });
 
+    //reset login page
+    this.authenticationService.logout();
+    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
+  }
+  get f() { return this.loginForm.controls }
+
+  onSubmit(){
+    this.submitted = true;
+    if(this.loginForm.invalid){
+      return;
+    }
+    this.loading = true;
+    this.authenticationService.login(this.f.username.value)
+        .pipe(first())
+        .subscribe(data =>{
+          this.router.navigate([this.returnUrl]);
+        }, 
+      error => {
+        this.error = error
+        this.loading = false
+      })
+  }
 }
